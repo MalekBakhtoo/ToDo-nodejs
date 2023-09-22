f5a49105b412b11173765d42e40c1c4831232fad
Malek Bakhtoo <bakhtoomalek81@gmail.com>
2023-09-22 17:12:13 +0330

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    await mongoose.connect('mongodb://127.0.0.1/ToDo')
+    await mongoose.connect(process.env.db)
       .then(() => console.log("connect to mongoose"))
       .catch((error) => console.log(error));
 };
